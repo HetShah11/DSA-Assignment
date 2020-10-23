a2aa5174ee603dde33de333d7f83eff9ae57f337
HetShah11 <68295185+HetShah11@users.noreply.github.com>
2020-10-23 22:49:29 +0530

    check the changes in code and add the use of height and prnt in struct in bst

--- rootfile.c
+++ rootfile.c
@@ -3,7 +3,7 @@
 typedef struct node
 {
 	int data, height; //h is height of tree
-	struct Node *left, *right, *parent;  // left child, right child and parent node
+	struct Node *left, *right, *prnt;  // left child, right child and parent node
 }Node;
 Node *rtAVL, *rtBST;         // root of AVL and BST
 
@@ -96,22 +96,99 @@ void search(Node** rt,int dat)
     }
 }
 
+Node* Succ(Node* curr)
+{
+    Node *s;
+    s=curr->right;
+    if (s!=NULL)
+    {
+        while (s->left!=NULL)
+            s=s->left;
+    }
+    return s;
+}
 
+void del_BST(Node **rt,int dat)
+{
+    Node *current, *parent=NULL;
 
+    current = *rt;
 
+    if (rt==NULL)
+    {
+        printf("\nNO DATA IN DIRECTORY");
+        return;
+    }
+
+    while (current!=NULL && ( dat!=current->data) )
+    {
+        parent = current;
+        if (dat<current->data)
+            current=current->left;
+        else
+            current=current->right;
+    }
+
+    if (current==NULL)
+        return;
+
+    if (current->left==NULL && current->right==NULL)
+    {
+        if (current!=*rt)
+        {
+            if (parent->left==current)
+                parent->left=NULL;
+            else
+                parent->right=NULL;
+        }
+        else
+            *rt = NULL;
+        free (current);
+    }
+
+    else if (current->left && current->right)
+    {
+        Node* suc = Succ(current);
+        int tem;
+        tem= suc->data;
+        delete(rt, tem);
+        current->data=tem;
+    }
+    else
+    {
+        Node* child = (current->left)?current->left:current->right;
+
+        if (current!=root)
+        {
+            if (current==parent->left)
+                parent->left=child;
+            else
+                parent->right=child;
+        }
+        else
+            *rt=child;
+        free(current);
+    }
+}
+
+void InorderDisplay(Node* rt)
+{
+    if (rt==NULL)
+        return;
+    AscendingDisplay(rt->left);
+    printf("%d",rt->data);
+    AscendingDisplay(rt->right);
+}
 
 
 
 
 
 void del_AVL(Node *x);    // Del of AVL node
-void del_BST(Node *x);    // Del of BST node
 void kink(Node *x);    // Resolving issue with the problematic grand child in AVL
 void Balance(Node *x); // for checking balance factor
 void lrot(Node *x);    // left rotate node in AVL
 void rrot(Node *x);    // right rotate
-Node* succ(Node *x);   // Finds successor node
-void print(Node *x);
 void init(Node *x)     // Initializing values of new node
 {
 	x=(Node*)malloc(sizeof(Node));
